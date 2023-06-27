cf260dbb9310ae59ff0f1d1fa4411b55a3304979
Martin Z <mzenze@gmail.com>
2023-06-27 18:38:14 -0300

    SVG components

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
+import { defineNuxtConfig } from 'nuxt/config'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
+  modules: [
+    'nuxt-svgo'
+  ],
+  imports: {
+    autoImport: false,
+  },
 })
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Home</h1>
+  <GitHubLogo />
+  <TwitterLogo />
+  <LinkedInLogo />
+</template>
+
+<script setup lang="ts">
+import GitHubLogo from 'assets/icons/social/github.svg'
+import TwitterLogo from 'assets/icons/social/twitter.svg'
+import LinkedInLogo from 'assets/icons/social/linkedin.svg'
+</script>
