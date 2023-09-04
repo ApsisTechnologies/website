cb2ef4f47b43de6cf48d9cf85986ea209aca0f23
Martin <martin@rockin.dev>
2023-09-04 16:04:44 -0300

    email assets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -30,11 +30,11 @@ export default defineNuxtConfig({
     autoImport: false
   },
   alias: {
-    'lib': resolve(__dirname, 'lib'),
-    'config': resolve(__dirname, 'config'),
-    'composables': resolve(__dirname, 'composables'),
-    'components': resolve(__dirname, 'components'),
-    'store': resolve(__dirname, 'store'),
+    lib: resolve(__dirname, 'lib'),
+    config: resolve(__dirname, 'config'),
+    composables: resolve(__dirname, 'composables'),
+    components: resolve(__dirname, 'components'),
+    store: resolve(__dirname, 'store'),
   },
   css: [
     'assets/css/typeface.css',
